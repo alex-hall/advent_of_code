159739e69301e2d15f9a3a2de12559f41fcd4910
Alex Hall <a.hall@calendly.com>
2021-12-02 10:09:41 -0500

    Day 1 solution!

--- day1/solution.ts
+++ day1/solution.ts
@@ -1,8 +1,18 @@
 import path from 'path';
 import fs from 'fs';
 
-const text = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf-8');
+function readFile(fileName: string) {
+    return fs.readFileSync(path.join(__dirname, fileName), 'utf-8');
+}
 
-const textAsArray = text.split("\n")
+const sweeps: string[] = readFile('./input.txt')
+    .split("\n")
 
-console.log(textAsArray[0])
+const increasingSweeps = sweeps.reduce((acc, current, index, array): number => {
+    let nextNumber = Number(array[index + 1]);
+    let currentNumber = Number(current);
+
+    return currentNumber < nextNumber ? acc + 1 : acc
+}, 0)
+
+console.log(`Sweeps increased ${increasingSweeps} times.`)
