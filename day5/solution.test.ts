592c8b4a8a663e78c1ef79e823c45c2e06915412
Alex Hall <a.hall@calendly.com>
2021-12-05 16:14:00 -0500

    This challenge had me going in the first half, not going to lie

--- day5/solution.test.ts
+++ day5/solution.test.ts
@@ -5,6 +5,6 @@ describe("part1", () => {
     it("Should work", () => {
 
         const rawFile = readFile("day5/numbers.txt")
-        expect(part1(rawFile) ).toEqual({solution: 5})
+        expect(part1(rawFile) ).toEqual({solution: 17193})
     })
 })
--- day5/solution.ts
+++ day5/solution.ts
@@ -17,6 +17,41 @@ function findStartingPoints(pointOne: number, pointTwo: number) {
     return pointOne < pointTwo ? {start: pointOne, end: pointTwo} : {start: pointTwo, end: pointOne}
 }
 
+function isDiagonal(x1: number, y1: number, x2: number, y2: number) {
+    return Math.abs(x1 - x2) === Math.abs(y1 - y2)
+}
+
+
+function diagonalLine(coordinateMatrix: number[][], x1: number, x2: number, y1: number, y2: number) {
+    let keepGoing = true
+    let cursorX = x1
+    let cursorY = y1
+
+    do {
+        coordinateMatrix[cursorX][cursorY] += 1
+
+        if (y1 > y2) {
+            cursorY -= 1
+        } else {
+            cursorY += 1
+        }
+
+        if (x1 > x2) {
+            cursorX -= 1
+        } else {
+            cursorX += 1
+        }
+
+        if (cursorX === x2 && cursorY === y2) {
+            coordinateMatrix[cursorX][cursorY] += 1
+            keepGoing = false
+        }
+    } while (keepGoing)
+
+
+    return coordinateMatrix
+}
+
 function plotLinesOnMatrix(coordinates: number[][][]) {
     let coordinateMatrix = instantiateMatrix(coordinates)
 
@@ -27,6 +62,9 @@ function plotLinesOnMatrix(coordinates: number[][][]) {
         } else if (y1 === y2) {
             let {start, end} = findStartingPoints(x1, x2);
             coordinateMatrix = plotHorizontalLine(coordinateMatrix, y1, start, end)
+        } else if (isDiagonal(x1, y1, x2, y2)) {
+            coordinateMatrix = diagonalLine(coordinateMatrix, x1, x2, y1, y2)
+
         }
     })
     return coordinateMatrix
