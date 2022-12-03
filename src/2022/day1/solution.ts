dcde9d883bd3fb5792a26e9976201b43ad29f151
Alex Hall <me@jalexhall.com>
2022-12-03 14:22:20 -0500

    Init array value when changing cursor

--- src/2022/day1/solution.ts
+++ src/2022/day1/solution.ts
@@ -1,10 +1,12 @@
 const part1 = (calorieList: string[]) : number => {
     let cursor = 0
     const summedCalorieByElf: number[] = calorieList.reduce((acc, curr, index, array): number[] => {
+
         if(curr.length){
             acc[cursor] += (+curr)
         }else{
             cursor +=1
+            acc[cursor] = 0
         }
         return acc
     }, [0])
