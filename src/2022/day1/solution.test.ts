838f1e620972ceee7d9c4b50702f6626f4ab9d75
Alex Hall <me@jalexhall.com>
2022-12-05 08:44:45 -0500

    Par Deaux

--- src/2022/day1/solution.test.ts
+++ src/2022/day1/solution.test.ts
@@ -1,4 +1,4 @@
-import {part1} from "./solution"
+import {part1, part2} from "./solution"
 import {readFile} from "common";
 describe("Part 1", () => {
     describe("Sample solution", () => {
@@ -7,5 +7,27 @@ describe("Part 1", () => {
 
             expect(part1(file)).toEqual(24000)
         })
+    })
+
+    describe("Puzzle solution", () => {
+        it("returns the highest calorie count", () => {
+            const file = readFile("2022/day1/input.txt")
+
+            expect(part1(file)).toEqual(71934)
+        })
+    })
+})
+
+describe("Part 2", () => {
+    describe("Sample solution", () => {
+        const file = readFile("2022/day1/sample.txt")
+
+        expect(part2(file)).toEqual(45000)
+    })
+
+    describe("Puzzle solution", () => {
+        const file = readFile("2022/day1/input.txt")
+
+        expect(part2(file)).toEqual(211447)
     })
 })
--- src/2022/day1/solution.ts
+++ src/2022/day1/solution.ts
@@ -1,19 +1,35 @@
-const part1 = (calorieList: string[]) : number => {
-    let cursor = 0
-    const summedCalorieByElf: number[] = calorieList.reduce((acc, curr, index, array): number[] => {
+const getSummedCalorieByElf = (calorieList: string[], cursor: number) => {
+    return calorieList.reduce((acc, curr): number[] => {
 
-        if(curr.length){
+        if (curr.length) {
             acc[cursor] += (+curr)
-        }else{
-            cursor +=1
+        } else {
+            cursor += 1
             acc[cursor] = 0
         }
         return acc
-    }, [0])
+    }, [0]);
+}
+
+const part1 = (calorieList: string[]) : number => {
+    let cursor = 0
+    const summedCalorieByElf: number[] = getSummedCalorieByElf(calorieList, cursor)
 
     return Math.max(...summedCalorieByElf)
 }
 
+const part2 = (calorieList: string[]) : number => {
+    let cursor = 0
+    const summedCalorieByElf: number[] = getSummedCalorieByElf(calorieList, cursor)
+
+    const sortedCaloriesByElf = summedCalorieByElf.sort((a,b) => b-a);
+    
+    return sortedCaloriesByElf[0] +
+        sortedCaloriesByElf[1] +
+        sortedCaloriesByElf[2]
+}
+
 export {
-    part1
+    part1,
+    part2
 }
